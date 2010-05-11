212358d97fbc38d677d486a1073af7d40d431c05
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-05-11 16:14:43 -0400

    Gist support with textile markup

--- src/todo/TodoMIDlet.java
+++ src/todo/TodoMIDlet.java
@@ -3,13 +3,17 @@ package todo;
 import java.util.*;
 import javax.microedition.midlet.*;
 import javax.microedition.lcdui.*;
+import javax.microedition.io.*;
+import java.io.*;
+
+import me.regexp.*;
 
 public class TodoMIDlet extends MIDlet implements CommandListener {
 
     private Display display;
-    private Form fmMain, fmAdd;
-    private Command cmNew, cmExit, cmCancel, cmCancelList, cmAdd, cmDone, cmCompleted;
-    private Vector vecTodo;
+    private Form fmMain, fmAdd, fmGist;
+    private Command cmNew, cmExit, cmCancel, cmCancelList, cmAdd, cmDone, cmCompleted, cmSend;
+    private Vector vecTodo, vecCompleted;
     private TextField tfTodo;
     private ChoiceGroup cgTodos;
     private List list;
@@ -17,25 +21,27 @@ public class TodoMIDlet extends MIDlet implements CommandListener {
     public TodoMIDlet() {
         display = Display.getDisplay(this);
         cmExit = new Command("Exit", Command.EXIT, 0);
-        display = Display.getDisplay(this);
 
         // Create 'main' and 'add todo item' forms
         // Form for setting prefs is in commandAction()
         fmMain = new Form("Todo List");
         fmAdd = new Form("New Todo");
+        fmGist = new Form("Send to Gist");
 
         // Todo list and vector
         cgTodos = new ChoiceGroup("", Choice.MULTIPLE);
         vecTodo = new Vector();
+        vecCompleted = new Vector();
 
         // Commands
         cmNew = new Command("New Todo", Command.SCREEN, 2);
         cmExit = new Command("Exit", Command.EXIT, 1);
         cmCancel = new Command("Main", Command.CANCEL, 1);
         cmCancelList = new Command("Main", Command.CANCEL, 1);
-        cmAdd = new Command("Add to List", Command.OK, 2);
+        cmAdd = new Command("Add to List", Command.OK, 1);
         cmDone = new Command("Mark Set as Done", Command.OK, 2);
         cmCompleted = new Command("View Completed", Command.SCREEN, 2);
+        cmSend = new Command("Send to Gist", Command.SCREEN, 2);
 
         // Text fields
         tfTodo = new TextField("Add:", "", 50, TextField.ANY);
@@ -45,6 +51,7 @@ public class TodoMIDlet extends MIDlet implements CommandListener {
         fmMain.addCommand(cmNew);
         fmMain.addCommand(cmDone);
         fmMain.addCommand(cmCompleted);
+        fmMain.addCommand(cmSend);
         fmMain.append(cgTodos);
         fmMain.setCommandListener(this);
 
@@ -56,6 +63,9 @@ public class TodoMIDlet extends MIDlet implements CommandListener {
         list = new List("Completed Todos", Choice.IMPLICIT);
         list.addCommand(cmCancelList);
         list.setCommandListener(this);
+
+        fmGist.addCommand(cmCancel);
+        fmGist.setCommandListener(this);
     }
 
     public void startApp() {
@@ -95,6 +105,10 @@ public class TodoMIDlet extends MIDlet implements CommandListener {
         if (c == cmCompleted) {
             display.setCurrent(list);
         }
+        if (c == cmSend) {
+            sendToGist();
+            display.setCurrent(fmGist);
+        }
     }
 
     public boolean addTodoItem(String item) {
@@ -121,9 +135,87 @@ public class TodoMIDlet extends MIDlet implements CommandListener {
         for (int i = cgTodos.size(); i > 0; i--) {
             if(cgTodos.isSelected(i - 1)) {
                 list.append(cgTodos.getString(i - 1), null);
+                vecCompleted.addElement(cgTodos.getString(i -1));
                 cgTodos.delete(i - 1);
             }
         }
+    }
+
+    public void sendToGist() {
+        String note = "*Todo*\n";
+        for (int i = 0; i < vecTodo.size(); i++) {
+            Object item = (String) vecTodo.elementAt(i);
+            note += item.toString() + "\n";
+        }
+
+        note += "\n*Completed*\n";
+        for (int i = 0; i < vecCompleted.size(); i++) {
+            Object item = (String) vecCompleted.elementAt(i);
+            note += "-" + item.toString() + "-";
+            if (i != vecCompleted.size()) {
+                note += "\n";
+            }
+        }
+        sendPostRequest(note);
+    }
+
+/*
+ * Sends a Post Request
+ *
+ * @link http://wiki.forum.nokia.com/index.php/How_to_use_Http_POST_request_in_Java_ME
+ */
+    public boolean sendPostRequest(String message) {
+	HttpConnection hc = null;
+	InputStream in = null;
+	OutputStream out = null;
+        String url = "http://gist.github.com/api/v1/xml/new";
+
+        message = "files[file.textile]=" + message;
+
+	// specifying the query string
+	try {
+            hc = (HttpConnection)Connector.open(url);
+            hc.setRequestMethod(HttpConnection.POST);
+            hc.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            hc.setRequestProperty("Content-Length", Integer.toString(message.length()));
+            out = hc.openOutputStream();
+            out.write(message.getBytes());
+            in = hc.openInputStream();
+            int length = (int)hc.getLength();
+            byte[] data = new byte[length];
+            in.read(data);
+            String response = new String(data);
+
+            // Pa
+            RE r = new RE("<repo>");
+            String[] arr = r.split(response);
+            r = new RE("</repo>");
+            arr = r.split(arr[1]);
+            StringItem gist = new StringItem(null, "http://gist.github.com/" + arr[0]);
+            StringItem text = new StringItem(null, "Your todo list has been saved to: ");
+
+            fmGist.deleteAll();
+            fmGist.append(text);
+            fmGist.append(gist);
+            fmGist.setTitle("Gist Saved!");
+
+	} catch (IOException ioe) {
+            StringItem stringItem = new StringItem(null, ioe.toString());
+            fmGist.append(stringItem);
+            fmGist.setTitle("Done");
+	} finally {
+            // freeing up i/o streams and http connection
+            try {
+		if (hc != null) hc.close();
+            } catch (IOException ignored) { }
+            try {
+		if (in != null) in.close();
+            } catch (IOException ignored) { }
+            try {
+                if (out != null) out.close();
+            } catch (IOException ignored) { }
+	}
+        return true;
     }
 
 }
